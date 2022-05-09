35ca61ed0752d9469884b1379cdee6d8fc923fe5
damlayagmur <dmlygmr@windowslive.com>
2022-05-09 21:25:39 +0300

    CountDownTimer added

--- app/src/main/java/com/damlayagmur/livedataexample/MainActivity.kt
+++ app/src/main/java/com/damlayagmur/livedataexample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.damlayagmur.livedataexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.damlayagmur.livedataexample.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var numberText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        numberText = findViewById(R.id.numberText)
+
+        viewModel.startTimer()
+        viewModel.seconds.observe(this, Observer {
+            numberText.text = it.toString()
+        })
     }
+
 }
---
+++ app/src/main/java/com/damlayagmur/livedataexample/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,33 @@
+package com.damlayagmur.livedataexample.viewmodel
+
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    private lateinit var timer: CountDownTimer
+
+    private val _seconds = MutableLiveData<Int>()
+    val seconds: LiveData<Int>
+        get() = _seconds
+
+    fun startTimer() {
+        timer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                val timeLeft = p0 / 1000
+                _seconds.value = timeLeft.toInt()
+            }
+
+            override fun onFinish() {
+                TODO("Not yet implemented")
+            }
+
+        }.start()
+    }
+
+    fun stopTimer() {
+        timer.cancel()
+    }
+}
